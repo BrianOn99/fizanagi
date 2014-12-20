0cc0973568c7c166a327fff3b7805da8133d591f
Chiu Yue Chun <chiubrian@cuhk.edu.hk>
2014-12-20 21:13:38 +0800

    fix lfn search with more than one dirent

--- src/readcluster.c
+++ src/readcluster.c
@@ -13,7 +13,7 @@
 #include "readcluster.h"
 #include "mylfn.h"
 
-#define DIRENT_SIZE 32
+#define DIRENT_SIZE sizeof(struct dirent)
 #define MAX_LFN_LEN 255
 
 void readcluster(struct fat_info *fatfs, void *buf, unsigned int index)
@@ -141,6 +141,19 @@ struct dirent *iterdirent(struct iterstate *state, char *name)
  * End iterator section
  * ******************************** */
 
+int skipstrcmp(const char *s1, const char *s2, int n)
+{
+        int m = n;
+        for ( ; *s1 == *s2; s1++, s2++, n--) {
+                if (m == 0) {
+                        m = n;
+                        continue;
+                } if (*s1 == '\0')
+                        return 0;
+        }
+        return 1;
+}
+
 int searchname(struct fat_info *fatfs, unsigned int cluster_i,
                   char *given_name, struct dirent back_dirents[], int len)
 {
@@ -159,7 +172,7 @@ int searchname(struct fat_info *fatfs, unsigned int cluster_i,
 
                 int match;
                 if (lfnstr[0]) {
-                        match = (strcmp(lfnstr+1, given_name+1) == 0);
+                        match = (skipstrcmp(lfnstr+1, given_name+1, CHAR_PER_LFN) == 0);
                 } else {
                         static char name8d3[11];
                         extract_8d3name(nextdirent, name8d3);
